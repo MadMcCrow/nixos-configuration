f95596d34efe356c675a941fe9ac41920c3aaa13
MadMcCrow <noe.perard@live.ru>
2022-09-23 23:40:43 +0200

    split audio

---
+++ modules/audio/default.nix
@@ -0,0 +1,5 @@
+# Audio can be handled with pulseaudio (NixOS default) or pipewire (the new multimedia standard)
+{ pkgs, config, lib, ... }:
+{
+	import = [./pipewire.nix]
+}
---
+++ modules/audio/pulse.nix
@@ -0,0 +1,16 @@
+# Audio can be handled with pulseaudio (NixOS default) or pipewire (the new multimedia standard)
+{ pkgs, config, lib, ... }:
+  # Pipewire
+  config = {
+      sound.enable = false; # disabled for pipewire
+      hardware.pulseaudio.enable = false; # disabled for pipewire
+      security.rtkit.enable = true; # rtkit is optional but recommended
+      services.pipewire = {
+        enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        pulse.enable = true;
+        jack.enable = true;
+      };
+    }
+}
