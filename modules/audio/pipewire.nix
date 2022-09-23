64d2d15506716de248284d5d7882debecbd95a12
MadMcCrow <noe.perard@live.ru>
2022-09-24 00:47:25 +0200

    fix missing curly brace

--- modules/audio/pipewire.nix
+++ modules/audio/pipewire.nix
@@ -1,5 +1,6 @@
 # Audio can be handled with pulseaudio (NixOS default) or pipewire (the new multimedia standard)
 { pkgs, config, lib, ... }:
+{
   # Pipewire
   config = {
       sound.enable = false; # disabled for pipewire
