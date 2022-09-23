4537aec8a52027a39b6601cfd4cc7dce2466c1dd
MadMcCrow <noe.perard@live.ru>
2022-09-24 00:47:25 +0200

    fix missing curly brace

--- modules/audio/default.nix
+++ modules/audio/default.nix
@@ -1,5 +1,5 @@
 # Audio can be handled with pulseaudio (NixOS default) or pipewire (the new multimedia standard)
 { inputs, lib, config, pkgs, impermanence, ... }:
 {
-	import = [./pipewire.nix];
+	imports = [./pipewire.nix];
 }
