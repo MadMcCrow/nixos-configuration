e18cc1f8bf152f15b85fe7d24da55e4e4e4cc666
MadMcCrow <noe.perard@live.ru>
2022-09-24 00:53:50 +0200

    fix zsh nix-shell plugin

--- modules/shell.nix
+++ modules/shell.nix
@@ -19,20 +19,31 @@
       update = "sudo nixos-rebuild switch";
       clean = "sudo nix-collect-garbage -d";
     };
-
-    plugins = [
-      # required to get zsh working in nix-shell
-      {
-        name = "zsh-nix-shell";
-        file = "nix-shell.plugin.zsh";
-        src = pkgs.fetchFromGitHub {
-          owner = "chisui";
-          repo = "zsh-nix-shell";
-          rev = "v0.5.0";
-          sha256 = "0za4aiwwrlawnia4f29msk822rj9bgcygw6a8a6iikiwzjjz0g91";
+    
+     oh-my-zsh = {
+          enable = true;
+          plugins = [ 
+          "git"
+          "zsh-nix-shell"
+          ];
+          theme = "robbyrussell";
         };
-      }
-    ];
+
+    # this is a home-manager only option : 
+    #plugins = [
+    #  # required to get zsh working in nix-shell
+    #  {
+    #    name = "zsh-nix-shell";
+    #
+    # file = "nix-shell.plugin.zsh";
+    #    src = pkgs.fetchFromGitHub {
+    #     owner = "chisui";
+    #      repo = "zsh-nix-shell";
+    #      rev = "v0.5.0";
+    # sha256 = "0za4aiwwrlawnia4f29msk822rj9bgcygw6a8a6iikiwzjjz0g91";
+    #    };
+    #  }
+    # ];
   };
 
   # make users default to zsh
