c34d5d84934efca1511cedcfcfb1feb96fc19a89
MadMcCrow <noe.perard@live.ru>
2022-09-24 14:56:11 +0200

    fix nix.nix

--- modules/nix.nix
+++ modules/nix.nix
@@ -1,6 +1,6 @@
 # nix.nix
 # 	Nix and nix store settings for nixos.
-{ pkgs, config}: {
+{ pkgs, config, ...}: {
   # Nix and flake support
   nix = {
     package = pkgs.nixFlakes; # or versioned attributes like nixVersions.nix_2_8
