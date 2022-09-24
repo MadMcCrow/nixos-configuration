278dacb9c4fda4e71609d336ef6aa0d587137686
MadMcCrow <noe.perard@live.ru>
2022-09-24 14:24:44 +0200

    clean and remove deprecated options

--- modules/flatpak.nix
+++ modules/flatpak.nix
@@ -1,19 +1,24 @@
-# flatpak
-#
-# TODO : use same persisting system as in persist.nix
+# flatpak.nix
+# 	Flatpak support for nixos
+#	Todo : move to a conditional setup
 { config, pkgs, impermanence, ...}: {
 
+# import thanks to specialArgs
 imports = [ impermanence.nixosModules.impermanence ];
+
   services.flatpak.enable = true;
   xdg.portal = {
     enable = true;
-    gtkUsePortal = true;
+    # deprecated : gtkUsePortal
+    # this can cause issues as 
+    # https://github.com/NixOS/nixpkgs/issues/135898
+    # https://github.com/NixOS/nixpkgs/issues/156950
+    # gtkUsePortal = true;
   };
 
-  # this folder is where the files will be stored (don't put it in tmpfs)
+  # this folder is where the files will be stored (don't put it in tmpfs/zfs clean partition)
+  # bind mounted from /persist/flatpak/var/lib/flatpak to /var/lib/flatpak
   environment.persistence."/nix/persist/flatpak" = {
-    directories = [
-      "/var/lib/flatpak" # bind mounted from /persist/flatpak/var/lib/flatpak to /var/lib/flatpak
-    ];
+    directories = ["/var/lib/flatpak"];
   };
 }
