cf2c19acbd390809e5f61f6893055712a28c7818
MadMcCrow <noe.perard@live.ru>
2022-09-24 15:00:10 +0200

    guest user update

--- modules/users/guest.nix
+++ modules/users/guest.nix
@@ -1,6 +1,6 @@
 # guest.nix
 # 	a custom, temporary user for whomever wants to be able to log on this computer
-{ config, pkgs, home-manager }: {
+{ config, pkgs, home-manager, ... }: {
 
   # imported thanks to specialArgs
   imports = [ home-manager.nixosModule ];
@@ -11,5 +11,13 @@
     home = "/home/guest";
     homeMode = "764";
     uid = 1001;
+  };
+
+  home-manager.users.guest = { lib, pkgs, ... }: {
+    home = with pkgs; {
+      packages = [ home-manager firefox-wayland ];
+      # just like for the base nixos configuration, do not touch
+      stateVersion = "22.05";
+    };
   };
 }
