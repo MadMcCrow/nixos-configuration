633a0b98121f7939a5f534288e22120e5ab87037
MadMcCrow <noe.perard@live.ru>
2022-09-29 21:01:40 +0200

    fix audio module

--- modules/audio/default.nix
+++ modules/audio/default.nix
@@ -2,5 +2,5 @@
 # 	Audio can be handled with pulseaudio (NixOS default) or pipewire (the new multimedia standard)
 # 	todo : make conditional setup with enum
 { inputs, lib, config, pkgs, ... }: {
-  imports = [ ./pipewire.nix /pulse.nix ];
+  imports = [ ./pipewire.nix ./pulse.nix ];
 }
