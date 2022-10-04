59c111093787a5ad47e01768b83a1d5dae1b841c
MadMcCrow <noe.perard@live.ru>
2022-10-05 00:04:28 +0200

    add pidgin messenger

---
+++ modules/apps/pidgin.nix
@@ -0,0 +1,35 @@
+# apps/pidgin.nix
+# 	pidgin chat app
+{ pkgs, config, lib, ... }:
+with builtins;
+with lib;
+let cfg = config.apps.pidgin;
+in {
+  options.apps.pidgin.enable = lib.mkOption {
+    type = types.bool;
+    default = false;
+    description = "enable pidgin :a multi protocol chat client";
+  };
+  config = lib.mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+    pidgin
+    pidgin-skypeweb
+    pidgin-opensteamworks
+    signald
+    ];
+    
+    services.bitlbee.libpurple_plugins = with pkgs; 
+    [
+    purple-slack
+    purple-discord
+    purple-matrix
+    telegram-purple
+    purple-matrix
+    libpurple-signald
+    ];
+    
+    # may be necessary
+    nixpkgs.config.allowUnfreePredicate = pkg:
+      builtins.elem (lib.getName pkg) [];
+  };
+}
