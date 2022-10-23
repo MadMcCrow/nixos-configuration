6dab42163b244e05224b8ad355ba6975b3523410
MadMcCrow <noe.perard@live.ru>
2022-10-23 20:56:03 +0200

    improve discord

--- modules/apps/discord.nix
+++ modules/apps/discord.nix
@@ -3,12 +3,24 @@
 { pkgs, config, lib, ... }:
 with builtins;
 with lib;
-let cfg = config.apps.discord;
+let
+  cfg = config.apps.discord;
+  # use OpenASAR for faster start-up times
+  OpenASAR = self: super: {
+    discord = super.discord.override { withOpenASAR = true; };
+  };
+  # Make sure to always download the latest tarball
+  # this cannnot work in pure evaluation because 
+  # you would need to know the sha256 in advance 
+  # when updating discord
+  # ForceUpdate = self: super: { discord = super.discord.overrideAttrs (_: { src = builtins.fetchTarball <link-to-tarball>;});};
 in {
+
   options.apps.discord.enable = lib.mkOption {
     type = types.bool;
     default = false;
     description = "enable discord : voice and text chat for gamers";
+
   };
   config = lib.mkIf cfg.enable {
     environment.systemPackages = with pkgs; [ discord nss_latest ];
@@ -16,8 +28,8 @@ in {
       config.allowUnfreePredicate = pkg:
         builtins.elem (lib.getName pkg) [ "discord" ];
 
-      # https://nixos.wiki/wiki/discord#Opening_Links_with_Firefox
-      # discord.override = with pkgs; { nss = nss_latest; };
+      # add our custom overlays
+      overlays = [ OpenASAR ];
     };
   };
 }
