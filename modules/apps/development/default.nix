ba76ec4ee790e3fe4f1a293d473b6c2062b8849d
MadMcCrow <noe.perard@live.ru>
2022-11-13 17:53:57 +0100

    add godot tool extension to vscode

--- modules/apps/development/default.nix
+++ modules/apps/development/default.nix
@@ -11,14 +11,14 @@ let
   debugtools.packages = with pkgs; [ nemiver sysprof ];
   # vs-code
   vscode.enable = cfg.codeEditor == "vscode" || cfg.codeEditor == "all";
-  vscode.module = import ./vscode.nix;
+  vscode.module = import ./vscode.nix { inherit config lib pkgs; };
+  vscode.packages = vscode.module.packages;
+  vscode.unfree = vscode.module.unfree;
   # lapce
   lapce.enable = cfg.codeEditor == "lapce" || cfg.codeEditor == "all";
   lapce.packages = with pkgs; [ lapce ];
 
 in {
-
-  # interface
   # interface
   options.apps.development = {
     enable = lib.mkOption {
@@ -53,10 +53,10 @@ in {
   config = lib.mkIf cfg.enable {
     environment.systemPackages = (with pkgs; [ rnix-lsp ])
       ++ (if debugtools.enable then debugtools.packages else [ ])
-      ++ (if lapce.enable then lapce.packages else [ ]);
-    #(if vscode.enable then vscode.module.vscode.packages else [ ])
+      ++ (if lapce.enable then lapce.packages else [ ])
+      ++ (if vscode.enable then vscode.packages else [ ]);
 
-    # unfree.unfreePackages = if vscode.enable then vscode.module.vscode.unfree else [ ];
+    unfree.unfreePackages = if vscode.enable then vscode.unfree else [ ];
   };
 
 }
--- modules/apps/development/vscode.nix
+++ modules/apps/development/vscode.nix
@@ -1,22 +1,19 @@
 # vscode.nix
 # 	Setup vscode and all it's things 
-{ config, pkgs, lib, unfree, ... }:
+{ config, pkgs, lib, ... }:
 with builtins;
 with lib;
 with pkgs.vscode-utils;
 let
-  # imports
-  inherit imports;
   # marketplace extensions
-  vscode.godotTools = (extensionFromVscodeMarketplace {
+  vsCodeGodotTools = (extensionFromVscodeMarketplace {
     name = "godot-tools";
     publisher = "geequlim";
     version = "1.3.1";
-    sha256 = "e344cdf2fc7b29e0444136991dd8a93efba9c4e9";
+    sha256 = "sha256-wJICDW8bEBjilhjhoaSddN63vVn6l6aepPtx8VKTdZA=";
   });
-
   # nixos extensions
-  vscode.extensions = with vscode-extensions; [
+  nixVsCodeExtensions = with pkgs.vscode-extensions; [
     jnoortheen.nix-ide
     ms-python.python
     rust-lang.rust-analyzer
@@ -25,15 +22,16 @@ let
     llvm-vs-code-extensions.vscode-clangd
   ];
 
+in {
   # final package
-  vscode.packages = with pkgs;
+  packages = with pkgs;
     [
       (vscode-with-extensions.override {
-        vscodeExtensions = vscode.extensions ++ vscode.godotTools;
+        vscodeExtensions = nixVsCodeExtensions ++ [ vsCodeGodotTools ];
       })
     ];
   # unfree predicate
-  vscode.unfree = [
+  unfree = [
     "vscode"
     "vscode-with-extensions"
     "jnoortheen.nix-ide"
@@ -44,6 +42,4 @@ let
     "llvm-vs-code-extensions.vscode-clangd"
     "vscode-extension-ms-vscode-cpptools"
   ];
-in {
-  # do nothing
 }
