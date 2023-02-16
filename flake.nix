898252c8afa7975e45900f48a6263d2dada85f85
MadMcCrow <noe.perard@live.ru>
2023-02-16 17:56:53 +0100

    fix missing bracket

--- flake.nix
+++ flake.nix
@@ -37,80 +37,76 @@
 
   };
 
-  outputs = { self, nixpkgs, darwin, home-manager, ... }@inputs: 
-  let 
-    inherit (darwin.lib) darwinSystem;
-  in
-  {
+  outputs = { self, nixpkgs, darwin, home-manager, ... }@inputs:
+    let inherit (darwin.lib) darwinSystem;
+    in {
 
-    #Linux : Nixos
-    nixosConfigurations = {
+      #Linux : Nixos
+      nixosConfigurations = {
 
-       # AF, my personal Desktop PC
-      nixAF = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = inputs;
-        modules = [
-          ./modules
-          ./systems/AF/configuration.nix
-          {
-            # desktop env
-            desktop.kde = {
-              enable = true;
-              extraApps = true;
-            };
-            # apps
-            apps = {
-              enable = true;
-              graphics.enable = true;
-              development.enable = true;
-              games.enable = true;
-              web.enable = true;
-              multimedia.enable = true;
-            };
-            # input
-            input.xone.enable = true;
-            # audio
-            audio.pipewire.enable = true;
-          }
-        ];
+        # AF, my personal Desktop PC
+        nixAF = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = inputs;
+          modules = [
+            ./modules
+            ./systems/AF/configuration.nix
+            {
+              # desktop env
+              desktop.kde = {
+                enable = true;
+                extraApps = true;
+              };
+              # apps
+              apps = {
+                enable = true;
+                graphics.enable = true;
+                development.enable = true;
+                games.enable = true;
+                web.enable = true;
+                multimedia.enable = true;
+              };
+              # input
+              input.xone.enable = true;
+              # audio
+              audio.pipewire.enable = true;
+            }
+          ];
+        };
+
+        # DreamCloud, my personal Local Server
+        DreamCloud = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = inputs;
+          modules = [
+            ./modules
+            ./systems/DreamCloud/configuration.nix
+            { nixos.enhancedSecurity.enable = true; }
+          ];
+        };
       };
 
-      # DreamCloud, my personal Local Server
-      DreamCloud = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = inputs;
-        modules = [
-          ./modules
-          ./systems/DreamCloud/configuration.nix
-          {
-            nixos.enhancedSecurity.enable = true;
-          }
-        ];
-
-    };
-
-    # MacOS
-    darwinSystems = rec {
-      # my MBA - that I need to rename
-      Noes-MacBook-Air = darwinSystem {
-        system = "aarch64-darwin";
-        modules = self.darwinModules ++ [ ./systems/MBA/configuration.nix ];
+      # MacOS
+      darwinSystems = rec {
+        # my MBA - that I need to rename
+        Noes-MacBook-Air = darwinSystem {
+          system = "aarch64-darwin";
+          modules = self.darwinModules ++ [ ./systems/MBA/configuration.nix ];
+        };
       };
-    };
 
-    # Overlay for apple-silicon, does not apply to linux
-    overlays = {
-      # Overlay useful on Macs with Apple Silicon
-        apple-silicon = final: prev: (prev.stdenv.system == "aarch64-darwin") {
-          # Add access to x86 packages system is running Apple Silicon
-          pkgs-x86 = import inputs.nixpkgs-unstable {
-            system = "x86_64-darwin";
-            inherit (nixpkgs) config;
+      # Overlay for apple-silicon, does not apply to linux
+      overlays = {
+        # Overlay useful on Macs with Apple Silicon
+        apple-silicon = final: prev:
+          (prev.stdenv.system == "aarch64-darwin") {
+            # Add access to x86 packages system is running Apple Silicon
+            pkgs-x86 = import inputs.nixpkgs-unstable {
+              system = "x86_64-darwin";
+              inherit (nixpkgs) config;
+            };
           };
-        }; 
       };
-  };
+    };
 }
-
 
