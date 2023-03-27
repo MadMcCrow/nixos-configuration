25a40da59955f5cec22e661a055efd525c25c81e
MadMcCrow <noe.perard+git@gmail.com>
2023-03-27 14:42:01 +0200

    Working install script

--- install-darwin.sh
+++ install-darwin.sh
@@ -10,12 +10,16 @@ fi
 
 install_nix()
 {
+  if ! [ -x "$(command -v nix)" ]; then
   # this script remove what blocks the reinstall of nix after an update
   clean_backup  bashrc
   clean_backup  zshrc
   clean_backup  bash.bashrc
   # launch installer
   curl -L https://nixos.org/nix/install | sh
+  else
+  echo "nix already installed, skipping"
+  fi
 }
 
 experimental_features()
@@ -32,20 +36,26 @@ experimental_features()
   fi
 }
  
+build()
+{
+  # build nix config on Darwin
+  host=${HOSTNAME/.local}
+  echo "building configuration for $host"
+  nix build ".#darwinSystems.$host.system"
+}
+
+apply()
+{
+  ./result/sw/bin/darwin-rebuild switch --flake 
+}
 
 # install nix if nix is not present
-if ! [ -x "$(command -v nix)" ]; then
-  install_nix
-fi
-
+install_nix
 # enable experimental features
 experimental_features
-
-# build nix config on Darwin
-# TODO : make this dynamic
-nix build '.#darwinSystems.Noes-MacBook-Air.system'
-
+# build MacOS configuration
+build
 # apply config
-./result/sw/bin/darwin-rebuild switch --flake 
+apply
 
-
+echo "Nix install done"
