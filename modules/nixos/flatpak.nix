5c50e62801b459a179d2f5ea431430856571e000
MadMcCrow <noe.perard+git@gmail.com>
2023-06-29 20:30:26 +0200

    xdg portal disabled if no desktop

--- modules/nixos/flatpak.nix
+++ modules/nixos/flatpak.nix
@@ -6,6 +6,7 @@ with lib;
 let
   nos = config.nixos;
   cfg = nos.flatpak;
+  dsk = config.desktop;
 in {
   # interface
   options.nixos.flatpak.enable = mkEnableOption (mdDoc "flatpak") // {
@@ -15,24 +16,21 @@ in {
   imports = [ impermanence.nixosModules.impermanence ];
   # configs
   config = lib.mkIf (nos.enable && cfg.enable) {
+   
+    # only enable if we harve a desktop environment
+    xdg.portal.enable = dsk.gnome.enable || dsk.kde.enable;
+
+    # enable package kit
+    services.packagekit.enable = true;
+    # enable flatpak
     services.flatpak.enable = true;
-    xdg.portal = {
-      enable = true;
-      # you might want to only enable one or the other
-      # extraPortals = with pkgs; [
-      #  xdg-desktop-portal-gtk
-      #  xdg-desktop-portal-kde
-      #];
-    };
+
 
     environment.systemPackages = with pkgs; [
       libportal
       libportal-gtk3
       packagekit
     ];
-
-    # enable package kit
-    services.packagekit.enable = true;
 
     # this folder is where the files will be stored (don't put it in tmpfs/zfs clean partition)
     # bind mounted from /persist/flatpak/var/lib/flatpak to /var/lib/flatpak
