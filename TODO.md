5df97371df9632f39d7c13c5fe882211ede422f8
MadMcCrow <noe.perard+git@gmail.com>
2023-11-03 13:50:52 +0100

    darwin users

--- TODO.md
+++ TODO.md
@@ -1,22 +1,26 @@
 # TODO :
 
-# Necessary :
- - [ ] Rewrite Linux modules for simplicity
+ - [ ] Users
+   - [ ] Option to enable or disable users by systems
+   - [ ] development tools in Users
  - [ ] map unfree programs to unfree automatically
  - [X] Flake checks for pre-commit
  - [X] secrets deployment
-    - [ ] autogen missing keys via service
- - [ ] Server
-    - [X] localhost nextcloud
-    - [ ] Use secrets
-    - [ ] Nextcloud on LAN
-    - [ ] Nextcloud in container
+    - [ ] MacOS support
+    - [ ] Simplify secrets and use agenix
+ - [ ] Linux
+   - [ ] Desktop
+      - [ ] Cinnamon without settings (dconf)
+      - [ ] Cinnamon extensions
+      - [ ] GTK theming
+   - [ ] Server
+      - [X] localhost nextcloud
+      - [ ] Use secrets
+      - [ ] Nextcloud on LAN
+      - [ ] Nextcloud in container
  - [ ] Make a documentation
  - [ ] Update github actions
 
  ## Possible :
  - [ ] Move nextcloud to a separate flake
- - [ ] Gnome changes
-   - [ ] Gnome as a flake
-   - [ ] Gnome install user-side
-   - [ ] simplify gnome and dconf usage
+ - [ ] Secrets as a separate flake
--- users/default.nix
+++ users/default.nix
@@ -1,76 +1,24 @@
 # users/default.nix
 # 	users for nixos and Darwin systems
 #   TODO : clean and simplify
-# TODO : split between MacOS and linux
-args@{ config, pkgs, lib, firefox-gnome-theme, ... }:
-with builtins;
-with lib;
+#   TODO : option to enable or disable users
+{ config, pkgs, lib, ... } @args :
 let
   # list of users :
   users = [ ./perard ];
 
-  # split "a.b" into ["a" "b"]
-  splitAttr = name: strings.splitString "." name;
-
-  # recursively get variables in lists
-  recurseGet = l: i: s:
-    if !(i < (length l)) then
-      s
-    else if (hasAttr (elemAt l i) s) then
-      (recurseGet l (i + 1) (getAttr (elemAt l i) s))
-    else
-      { };
-
-  # merger function
-  cat = a: b:
-    if (isAttrs a) then
-      (a // b)
-    else if (isList a) then
-      concatLists [ a b ]
-    else
-      b;
-  recMerge = l: i: s:
-    if (i < (length l)) then recMerge l (i + 1) (cat s (elemAt l i)) else s;
-  merge = l: recMerge l 0 (elemAt l 0);
-
-  # function to merge variables
-  keyMerge = key: list: merge (map (x: recurseGet (splitAttr key) 0 x) list);
-
-  # imports
-  userList = map (x: import x args) users;
-
+  
   # merge users :
-  configUsers = keyMerge "users.users" userList;
-  home-managerUsers = keyMerge "home-manager.users" userList;
-
-  # only M1 supported for now
-  isDarwin = pkgs.system == "aarch64-darwin";
-
-  # minimal config users
-  darwinUser = mapAttrs (name: value: {
-    inherit name;
-    home = "/Users/${name}";
-  }) configUsers;
-
-  darwinHM = mapAttrs (name: value:
-    let homeValue = value args;
-    in (args:
-      homeValue // {
-        home = homeValue.home // { homeDirectory = "/Users/${name}"; };
-      })) home-managerUsers;
+  usersConfig = lib.attrsets.zipAttrsWith
+  (name: value: builtins.foldl' (a: b: a//b) {} value)
+  (map (x: (import x args)) users);
 
 in {
-  config = {
-    # default nix config users :
-    users.users = if isDarwin then darwinUser else configUsers;
-    # users.mutableUsers = true;
-
+  config = ({
     # home manager config users :
     home-manager = {
-      extraSpecialArgs = { inherit firefox-gnome-theme; };
       useGlobalPkgs = true;
       useUserPackages = true;
-      users = if isDarwin then darwinHM else home-managerUsers;
     };
-  };
+  } // usersConfig);
 }
