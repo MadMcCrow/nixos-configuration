1eb130ecbf5f63224c85aba5fd3f9368bcef8e7e
MadMcCrow <noe.perard+git@gmail.com>
2024-06-10 22:51:16 +0200

    main : disable NUC server

--- systems/NUC/default.nix
+++ systems/NUC/default.nix
@@ -2,5 +2,9 @@
 #   this is a 12th gen Intel NUC
 #   it's my central Home Cloud
 { ... }: {
-  imports = [ ./configuration.nix ./hardware-configuration.nix ./server.nix ];
+  imports = [ 
+    ./configuration.nix 
+    ./hardware-configuration.nix
+    # ./server.nix
+  ];
 }
