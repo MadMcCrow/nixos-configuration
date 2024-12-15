72a6ee9c2942ec5e85f6b52426cc25c43e964685
MadMcCrow <noe.perard+git@gmail.com>
2024-12-15 18:42:15 +0100

    fix: clean games module

--- modules/games/default.nix
+++ modules/games/default.nix
@@ -11,7 +11,7 @@ in
     enable = mkDisableOption "game support on nixos";
     xbox.enable = mkDisableOption "xbox hardware (controller, dongle, ...)";
     valve = {
-      enable = mkDisableOption "steam, the video game service";
+      enable = mkEnableOption "steam, the video game service";
       firewall.enable = mkDisableOption "open firewall for steam games";
     };
   };
@@ -22,19 +22,32 @@ in
         inherit (cfg.xbox) enable;
       };
       steam-hardware = {
-        inherit (cfg.valve) enable; # Steam udev rules
-      };
+        inherit (cfg.valve) enable;
+      }; # Steam udev rules
     };
 
-    nixos.nix = {
-      unfreePackages =
-        with lib.list;
-        (optional cfg.xbox.enable "xow_dongle-firmware")
-        ++ (optional cfg.valve.enable "steam-original");
+    nixos.nix.unfreePackages =
+      with lib.lists;
+      (optional cfg.valve.enable "steam-original")
+      ++ (optional cfg.xbox.enable "xow_dongle-firmware");
+
+    networking.firewall = lib.mkIf cfg.valve.enable {
+      allowedTCPPorts = [
+        27015 # remote play
+        27036 # SRCDS Rcon port
+      ];
+      allowedUDPPorts = [ 27015 ]; # Gameplay traffic
+      allowedUDPPortRanges = [
+        {
+          from = 27031;
+          to = 27036;
+        }
+      ]; # remote play
     };
 
-    # gaming :
+    # 
     programs = {
+      # gamemode improves performances
       gamemode = {
         enable = true;
         enableRenice = true;
@@ -49,31 +62,17 @@ in
             renice = 10;
           };
           # test before changing
-          gpu = {
-            # apply_gpu_optimisations = "accept-responsibility";
-            #gpu_device = 1;
-            #amd_performance_level = "high";
-          };
+          #gpu = {
+          # apply_gpu_optimisations = "accept-responsibility";
+          #gpu_device = 1;
+          #amd_performance_level = "high";
+          # };
         };
-        # Reimplementation for Steam Controller on Wayland
-        steam.extest.enable = cfg.valve.enable;
-
       };
-
-      networking.firewall = lib.mkIf cfg.valve.firewall.enable {
-        allowedTCPPorts = [
-          27015 # remote play
-          27036 # SRCDS Rcon port
-        ];
-        allowedUDPPorts = [ 27015 ]; # Gameplay traffic
-        allowedUDPPortRanges = [
-          {
-            from = 27031;
-            to = 27036;
-          }
-        ]; # remote play
+      # Reimplementation for Steam Controller on Wayland
+      steam.extest = {
+        inherit (cfg.valve) enable;
       };
-
     };
   };
 }
