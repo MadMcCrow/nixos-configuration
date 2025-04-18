472fb1744fd9f3283ce3edf0e30030f81f014e36
MadMcCrow <noe.perard+git@gmail.com>
2025-04-18 13:16:09 +0200

    dev: succesffully use poetry with nix

--- packages/termcolors/default.nix
+++ packages/termcolors/default.nix
@@ -2,7 +2,8 @@
 with python311Packages;
 buildPythonApplication {
   pname = "termcolors";
-  version = "1.0";
-  buildInputs = [ ];
+  version = "0.1.0";
+  pyproject = true;
+  nativeBuildInputs = [ poetry-core ];
   src = ./.;
 }
