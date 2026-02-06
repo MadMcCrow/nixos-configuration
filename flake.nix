1c28627845ff5252e2b7a99d0799518dae8c9834
MadMcCrow <noe.perard+git@gmail.com>
2026-02-06 21:30:26 +0100

    removed packages

--- flake.nix
+++ flake.nix
@@ -37,18 +37,18 @@
       inherit (systems) nixosConfigurations darwinConfigurations;
 
       # support packages :
-      packages =
-        nixpkgs.lib.genAttrs
-          [
-            "x86_64-linux"
-            "aarch64-darwin"
-          ]
-          (
-            system:
-            let
-              pkgs = nixpkgs.legacyPackages.${system};
-            in
-            pkgs.callPackages ./packages { }
-          );
+      #packages =
+      #  nixpkgs.lib.genAttrs
+      #    [
+      #      "x86_64-linux"
+      #      "aarch64-darwin"
+      #    ]
+      #    (
+      #      system:
+      #      let
+      #        pkgs = nixpkgs.legacyPackages.${system};
+      #      in
+      #      pkgs.callPackages ./packages { }
+      #   );
     };
 }
