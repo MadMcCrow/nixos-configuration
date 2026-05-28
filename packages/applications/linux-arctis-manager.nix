74a4e1e9920584364300cdf4e8a1931b62f41c4a
MadMcCrow <noe.perard+git@gmail.com>
2026-05-28 12:30:37 +0200

    WIP: add arctis manager package

--- packages/applications/linux-arctis-manager.nix
+++ packages/applications/linux-arctis-manager.nix
@@ -2,12 +2,12 @@
 # a cool app to manage Steelseries headsets
 {
   lib,
-  python312Packages,
+  pkgs,
   fetchFromGitHub,
   ...
 }@ args:
 let
-  pythonPkgs = python312Packages;
+  pythonPkgs = pkgs.python311Packages;
   version = "2.3.1";
   description = "A replacement for SteelSeries GG software, to manage your Arctis device on Linux!";
   name = "Linux-Arctis-Manager";
@@ -21,11 +21,12 @@ pythonPkgs.buildPythonPackage {
     inherit owner;
     repo ="${name}";
     rev = "v${version}";
+    hash = "sha256-eeh2cUOLuezPKI+QdaYMgthaBOv/ccSoBEFJ50LZ59c=";
   };
 
   pyproject = true;
   build-system = [ pythonPkgs.uv-build ];
-  propagatedBuildInputs = with pythonPkgs; [ uv autoflake ];
+  propagatedBuildInputs = with pythonPkgs; [ uv uv-build autoflake ];
 
   meta = {
     homepage = "https://github.com/${owner}/${name}";
